6784d18003fd0a03db137d15c8e768b4b45c06cd
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-05-18 18:32:40 -0300

    feat: Config database.

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,12 @@
+const connection = require('../config/db');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+connection();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`🛸 Listening on port ${port}`));
